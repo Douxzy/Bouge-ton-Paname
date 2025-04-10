6fc05aa50c2216287ccd8e93fa67df15665711d5
Douxzy <romain.descha2006@gmail.com>
2025-04-10 13:30:29 +0200

    Fix/ajout du lien admin dans le footer

--- www/vue/footer/footer.php
+++ www/vue/footer/footer.php
@@ -14,6 +14,7 @@
                 <li><a href="<?php __DIR__; ?>index.php" class="hover:text-white transition">Accueil</a></li>
                 <li><a href="<?php __DIR__; ?>mentions-legales.php" class="hover:text-white transition">Mentions Legales</a></li>
                 <li><a href="<?php __DIR__; ?> contact.php" class="hover:text-white transition">Contact</a></li>
+                <li><a href="../admin/login.php" class="hover:text-white transition">Admin</a></li>
             </ul>
         </div>
 
